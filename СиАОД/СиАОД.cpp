360696c89320008a61845b0c0bc682ab4faca8c3
Калинин Вадим <vadcka.casper@yandex.ru>
2019-11-22 13:08:05 +0300

    The third part of the course is ready

--- СиАОД/СиАОД.cpp
+++ СиАОД/СиАОД.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
 #include <fstream>
 #include "locale.h"; 
 using namespace std;
+
+
 
 struct record
 {
@@ -67,10 +69,10 @@ void add_spis()
 
 void Open_base()
 {
-
+	p.tail = p.head;
 	int i = 0;
 	int g = 0;
-	while (p.head != NULL)
+	while (p.tail != NULL)
 	{
 		cout << "1 / 0" << endl;
 		int temp;
@@ -78,18 +80,18 @@ void Open_base()
 		if (temp == 1)
 		{
 			int n = 0;
-			while ((p.head != p.tail) & (n < 500))
+			while ((p.tail != nullptr) & (n < 500))
 			{
 
 				cout << i + 1 << ")";
-				cout << p.head->str.Depositor << "   "
-					<< p.head->str.Amount << "   "
-					<< p.head->str.Date << "   "
-					<< p.head->str.Lawyer << "   "
+				cout << p.tail->str.Depositor << "   "
+					<< p.tail->str.Amount << "   "
+					<< p.tail->str.Date << "   "
+					<< p.tail->str.Lawyer << "   "
 					<< endl;
 				n++;
 				i++;
-				p.head = p.head->next;
+				p.tail = p.tail->next;
 			}
 		}
 		else
@@ -97,17 +99,17 @@ void Open_base()
 			if (temp == 0)
 				break;
 			else
-				while (p.head != NULL)
+				while (p.tail != nullptr)
 				{
 
 					cout << i + 1 << ")";
-					cout << p.head->str.Depositor << "   "
-						<< p.head->str.Amount << "   "
-						<< p.head->str.Date << "   "
-						<< p.head->str.Lawyer << "   "
+					cout << p.tail->str.Depositor << "   "
+						<< p.tail->str.Amount << "   "
+						<< p.tail->str.Date << "   "
+						<< p.tail->str.Lawyer << "   "
 						<< endl;
 					i++;
-					p.head = p.head->next;
+					p.tail = p.tail->next;
 				}
 
 		}
@@ -158,41 +160,73 @@ void Indexed_array(elem** head, elem** tail, record**& arr)
 
 
 }
+//:::::::::::::::::::::::::::::::::::Дерево::::::::::::::::::::::::::::::::::::::::::::::::::
 
-void Search_Binary(record**& arr, int lef, int righ)
+int VR = 1, HR = 1;
+void D_B_D(record*& x, Vertex*& p)
 {
-	int key = 0;
-	int midd = 0;
-	cout << "Enter the item you want to find ";
-	cin >> key;
-	while(lef < righ)
+
+	if (p == nullptr)
 	{
-		midd = (lef + righ) / 2;
-		if ((arr[midd]->Amount) < key)
-			lef = midd + 1;
-		else
-			righ = midd;
+		p = new Vertex;
+
+		p->data = x;
+		p->left = p->right = nullptr;
+		p->Balance = 0;
+		VR = 1;
 	}
-	if (key == 10000 || key == 20000 || key == 30000 || key == 40000)
-		midd++;
-	if (arr[midd]->Amount == key)
+	else if (strcmp(p->data->Depositor, x->Depositor) > 0)
 	{
-		int i = 1;
-		while (arr[midd]->Amount == key)
+		//cout << p->data->Depositor << " > " << x->Depositor << endl;
+		D_B_D(x, p->left);
+		if (VR == 1)
 		{
-			
-			cout << i << ") " << arr[midd]->Depositor << "   "
-				<< arr[midd]->Amount << "    "
-				<< arr[midd]->Date << "    "
-				<< arr[midd]->Lawyer
-				<< endl;
-			i++;
-			midd++;
+			if (p->Balance == 0)
+			{
+				Vertex* q;
+				q = p->left;
+				p->left = q->right;
+				q->right = p;
+				p = q;
+				q->Balance = 1;
+				VR = 0;
+				HR = 1;
+
+			}
+			else
+			{
+				p->Balance = 0;
+				VR = 1;
+				HR = 0;
+			}
 		}
+		else
+			HR = 0;
 	}
-	else
-	{
-		cout << "Element's " << key << " not found!" << endl;
+	else if (strcmp(p->data->Depositor, x->Depositor) < 0) {
+		//cout << p->data->Depositor << " < " << x->Depositor << endl;
+		D_B_D(x, p->right);
+		if (VR == 1)
+		{
+			p->Balance = 1;
+			HR = 1;
+			VR = 0;
+		}
+		else if (HR == 1)
+			if (p->Balance == 1)
+			{
+				Vertex* q;
+				q = p->right;
+				p->Balance = 0;
+				q->Balance = 0;
+				p->right = q->left;
+				q->left = p;
+				p = q;
+				VR = 1;
+				HR = 0;
+			}
+			else
+				HR = 0;
 	}
 }
 
@@ -221,96 +255,215 @@ int size_tree(Vertex* vertex)
 
 }
 
-int VR = 1, HR = 1;
-
-void D_B_D(record*& x, Vertex*& p)
+double sum_height(Vertex* vertex, int l)
 {
-	if (p == nullptr)
-	{
-		p = new Vertex;
-		
-		p->data = x;
-		p->left = p->right = nullptr;
-		p->Balance = 0;
-		VR = 1;
-	}
-	else if (p->data->Depositor > x->Depositor)
-	{
-		D_B_D(x, p->left);
-		if (VR == 1)
-		{
-			if (p->Balance == 0)
-			{
-				Vertex* q;
-				q = p->left;
-				p->left = q->right;
-				q->right = p;
-				p = q;
-				q->Balance = 1;
-				VR = 0;
-				HR = 1;
+	if (vertex == nullptr)
+		return 0;
+	else
+		return l + sum_height(vertex->left, l + 1) + sum_height(vertex->right, l + 1);
+}
 
-			}
-			else
-			{
-				p->Balance = 0;
-				VR = 1;
-				HR = 0;
-			}
-		}
-		else
-			HR = 0;
-	}
-	else if (p->data->Depositor < x->Depositor) {
-		D_B_D(x, p->right);
-		if (VR == 1)
-		{
-			p->Balance = 1;
-			HR = 1;
-			VR = 0;
-		}
-		else if (HR == 1)
-			if (p->Balance == 1)
-			{
-				Vertex* q;
-				q = p->right;
-				p->Balance = 0;
-				q->Balance = 0;
-				p->right = q->left;
-				q->left = p;
-				p = q;
-				VR = 1;
-				HR = 0;
-			}
-			else
-				HR = 0;
+void del(Vertex* root)
+{
+	if (root)
+	{
+		del(root->left);
+		del(root->right);
+		delete root;
 	}
 }
 
-void tree(record**&arr, Vertex*root)
+void Search_tree(Vertex* root, string x)
 {
-for (size_t i = 0; i < 4000; i++)
+
+	if (root)
 	{
-		D_B_D(arr[i], root);
+		string str(root->data->Depositor, sizeof(char) * x.length());
+		//cout << str << endl;
+
+		if (x < str)
+
+		{//cout << "L" << endl;
+			Search_tree(root->left, x);
+		}
+		if (x > str)
+		{
+			//cout << "R" << endl;
+			Search_tree(root->right, x);
+		}
+		if (x == str)
+		{
+
+			cout << root->data->Depositor << "   "
+				<< root->data->Amount << "   "
+				<< root->data->Date << "   "
+				<< root->data->Lawyer << endl;
+			Search_tree(root->left, x);
+			Search_tree(root->right, x);
+		}
 	}
-	print(root);
-	cout <<endl << size_tree(root);
+}
+
+int prompt_submenu_item()
+{
+	int varian;
+	cout << endl << "Working with wood\n" << endl;
+	/* << "1. To create a tree\n"*/
+	cout << "1. Display the tree\n"
+		<< "2. Search for an item in the tree\n"
+		<< "3. Indicators of tree\n"
+		<< "4. Exit\n" << endl;
+	cout << ">>> ";
+	cin >> varian;
+	return varian;
+}
+
+int tree(Vertex* root)
+{
+	string temp;
+	while (1)
+	{
+		int varian = prompt_submenu_item();
+		switch (varian) {
+			/*case 1:
+				system("cls");
+				for (int i = 0; i < 4000; i++)
+					{
+					/*cout << i << ") " << arr[i]->Depositor << "   "
+						<< arr[i]->Amount << "    "
+						<< arr[i]->Date << "    "
+						<< arr[i]->Lawyer
+						<< endl;
+						D_B_D(arr[i], root);
+					}
+				break;*/
+		case 1:
+			system("cls");
+			print(root);
+			break;
+		case 2:
+			system("cls");
+			cin >> temp;
+			Search_tree(root, temp);
+			break;
+
+		case 3:
+			system("cls");
+			cout << endl << "Size tree " << size_tree(root) << endl;
+			cout << endl << "Midd_height: " << sum_height(root, 1) / size_tree(root) << endl;
+			break;
+		case 4:
+			system("cls");
+			return 0;
+			break;
+		default:
+			system("cls");
+			cout << "You chose the wrong option" << endl;
+			break;
+		}
+	}
+}
+
+void Search_Binary(record**& arr, int lef, int righ, Vertex* root)
+{
+	int key = 0;
+	int midd = 0;
+	cout << "Enter the item you want to find ";
+	cin >> key;
+	while(lef < righ)
+	{
+		midd = (lef + righ) / 2;
+		if ((arr[midd]->Amount) < key)
+			lef = midd + 1;
+		else
+			righ = midd;
+	}
+	if (key == 10000 || key == 20000 || key == 30000 || key == 40000)
+		midd++;
+	if (arr[midd]->Amount == key )
+	{
+		
+		int i = 1;
+		while (arr[midd]->Amount == key  )
+		{
+			
+			cout << i << ") " << arr[midd]->Depositor << "   "
+				<< arr[midd]->Amount << "    "
+				<< arr[midd]->Date << "    "
+				<< arr[midd]->Lawyer
+				<< endl;
+
+			i++;
+			
+			D_B_D(arr[midd], root);
+			midd++;
+			if (midd == 4000)
+			{
+				
+				break;
+			}
+		}
+		
+	}
+	else
+	{
+		cout << "Element's " << key << " not found!" << endl;
+	}
+	tree( root);
+	del(root);
 }
 	
+int prompt_menu_item()
+{
+	int variant;
+	cout <<endl<< "Selected option\n" << endl;
+	cout << "1. Sorted list\n"
+		<< "2. Display list\n"
+		<< "3. To create an indexed array\n"
+		<< "4. Binary search\n"
+		<< "5. Exit\n" << endl;
+	cout << ">>> ";
+	cin >> variant;
+	return variant;
+}
+
+
 
 int main()
 {
 
-	record** arr = new record*[4000];
+	record** arr = new record * [4000];
 	Vertex* root = nullptr;
 	p.head = NULL;
 	p.tail = NULL;
 	add_spis();
-	Digital_sort(&p.head, &p.tail);
-	Indexed_array(&p.head, &p.tail, arr);
-	//Open_base();
-	//Search_Binary(arr, 0, 4000);
-	tree(arr, root);
-	
+
+	while (1)
+	{
+		
+		int variant = prompt_menu_item();
+		system("cls");
+		switch (variant) {
+		case 1:
+			Digital_sort(&p.head, &p.tail);
+			break;
+		case 2:
+			Open_base();
+			break;
+		case 3:
+			Indexed_array(&p.head, &p.tail, arr);
+			break;
+		case 4:
+			Search_Binary(arr, 0, 4000,root);
+			break;
+		
+		case 5:
+			exit(EXIT_SUCCESS);
+			break;
+		default:
+			cout << "You chose the wrong option" << endl;
+			exit(EXIT_FAILURE);
+		}
+	}
 	return 0;
 }
